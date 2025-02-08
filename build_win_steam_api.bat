7991a012f60adaa22e77d4518c10972a53e6aca2
Ayrzo <ayrzo@lurito.cn>
2025-02-08 19:13:50 +0800

    We have released version v1.0!
    In this submission, we added the rc file during the dll
    compilation process, so now the compiled dll file can
    view the version number information in the properties.

--- build_win_steam_api.bat
+++ build_win_steam_api.bat
@@ -9,18 +9,32 @@ mkdir release
 call build_set_protobuf_directories.bat
 if not exist build mkdir build
 
+REM ================ x86 build ================
+
 "%PROTOC_X86_EXE%" -I.\dll\ --cpp_out=.\dll\ .\dll\net.proto
 call build_env_x86.bat
 cd build
+
+REM build .rc file
+rc /fo dll_resources.res ..\assets\dll_resources.rc
+
+REM build .dll
 cl ../dll/rtlgenrandom.c ../dll/rtlgenrandom.def
-cl /LD /DEMU_RELEASE_BUILD /DNDEBUG /wd4828 /I%PROTOBUF_X86_DIRECTORY%\include\ ../dll/*.cpp ../dll/*.cc "%PROTOBUF_X86_LIBRARY%" Iphlpapi.lib Ws2_32.lib rtlgenrandom.lib Shell32.lib User32.lib /EHsc /MP12 /Ox /GL /Gy /link /debug:none /OUT:..\release\steam_api.dll
+cl /LD /DEMU_RELEASE_BUILD /DNDEBUG /wd4828 /I%PROTOBUF_X86_DIRECTORY%\include\ ../dll/*.cpp ../dll/*.cc "%PROTOBUF_X86_LIBRARY%" Iphlpapi.lib Ws2_32.lib rtlgenrandom.lib Shell32.lib User32.lib /EHsc /MP12 /Ox /GL /Gy dll_resources.res /link /debug:none /OUT:..\release\steam_api.dll
 cd "%~dp0"
+
+REM ================ x64 build ================
 
 "%PROTOC_X64_EXE%" -I.\dll\ --cpp_out=.\dll\ .\dll\net.proto
 call build_env_x64.bat
 cd build
+
+REM build .rc file
+rc /fo dll_resources.res ..\assets\dll_resources.rc
+
+REM build .dll
 cl ../dll/rtlgenrandom.c ../dll/rtlgenrandom.def
-cl /LD /DEMU_RELEASE_BUILD /DNDEBUG /wd4828 /I%PROTOBUF_X64_DIRECTORY%\include\ ../dll/*.cpp ../dll/*.cc "%PROTOBUF_X64_LIBRARY%" Iphlpapi.lib Ws2_32.lib rtlgenrandom.lib Shell32.lib User32.lib /EHsc /MP12 /Ox /GL /Gy /link /debug:none /OUT:..\release\steam_api64.dll
+cl /LD /DEMU_RELEASE_BUILD /DNDEBUG /wd4828 /I%PROTOBUF_X64_DIRECTORY%\include\ ../dll/*.cpp ../dll/*.cc "%PROTOBUF_X64_LIBRARY%" Iphlpapi.lib Ws2_32.lib rtlgenrandom.lib Shell32.lib User32.lib /EHsc /MP12 /Ox /GL /Gy dll_resources.res /link /debug:none /OUT:..\release\steam_api64.dll
 cd "%~dp0"
 
 copy Readme_release.txt release\Readme.txt
